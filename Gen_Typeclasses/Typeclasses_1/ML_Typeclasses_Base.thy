604da5e2da2f11e7e7ae6944c62510a8107e351f
Kevin Kappelmann <accounts@kappelmann.me>
2025-11-10 15:30:35 +0100

    feat(*) update to latest AFP devel

--- Gen_Typeclasses/Typeclasses_1/ML_Typeclasses_Base.thy
+++ Gen_Typeclasses/Typeclasses_1/ML_Typeclasses_Base.thy
@@ -5,11 +5,9 @@ theory ML_Typeclasses_Base
     ML_Eval_Antiquotation
 begin
 
-declare [[ParaT_args args = ['p1] and sep = ", " and encl = "" ", " and encl_arg = "" "" and stop = ]]
+declare [[ParaT_args args: ['p1] sep: ", " encl: "" ", " encl_arg: "" "" stop: ]]
 
-ML\<open>
-  val sfx_ParaT_nargs = ML_Gen.sfx_ParaT_nargs
-\<close>
+ML\<open>val sfx_ParaT_nargs = ML_Gen.sfx_ParaT_nargs\<close>
 
 ML_gen_file\<open>typeclass_base.ML\<close>
 ML_gen_file\<open>typeclass_base_instance.ML\<close>
